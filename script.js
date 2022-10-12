4b012f4ab6290b27195822894dfaa2d44b0f13ac
damienvanlier <9014287@student.zadkine.nl>
2022-10-12 13:28:03 +0200

    opzet opdracht 6.1

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function ChangeColor () {
+alert("Het werkt");
+console.log("Het werkt in de console!");
+}
+function calculator(num, num2) {
+ let answer = num + num2;
+ console.log(answer);
+alert (answer);
+}
+
+
+
+
+
+
